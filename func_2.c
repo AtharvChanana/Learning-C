a09bb1b0634b80da5e6f67758b52628376f4cfb1
Atharv Chanana <raaghav5616@gmail.com>
2024-09-08 16:05:58 +0530

    Create func_2.c

---
+++ func_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+    void good_morning();
+    void good_afternoon();
+    void good_night();
+
+    void good_morning(){
+        printf("good morning\n");
+    }
+    void good_afternoon(){
+        printf("good afternoon\n");
+    }
+    void good_night(){
+        printf("good night\n");
+    }
+int main(){
+    good_morning();
+    good_afternoon();
+    good_night();
+    return 0;
+}
