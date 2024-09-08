c712b1c381d9059b9b10e09b0b53fffd0633e7ca
Atharv Chanana <raaghav5616@gmail.com>
2024-09-08 16:06:37 +0530

    Create math_lib.c

---
+++ math_lib.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a;
+    printf("enter a number :");
+    scanf("%d",&a);
+    
+    printf("the square of the number is %f",pow(a,2));
+    return 0;
+}
